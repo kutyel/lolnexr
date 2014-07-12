60c4b32d8c898f078645314193887d25535381ed
kutyel <flaviocorpa@gmail.com>
2014-07-12 15:03:30 +0200

    App start

---
+++ packages/jQuery-win8.1.8.2/tools/Install.ps1
@@ -0,0 +1,18 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+function Set-ItemTypeNone {
+  param($item)
+  
+  $item.Properties.Item("{ItemType}").Value = "None"
+}
+
+$jsItem = $project.ProjectItems.Item("js")
+
+$jQuerySource         = $jsItem.ProjectItems.Item("jquery-1.8.2-win8-1.0.js")
+$jQueryVsdoc          = $jsItem.ProjectItems.Item("jquery-1.8.2-vsdoc.js")
+$jQueryDeferredSource = $jsItem.ProjectItems.Item("jquery-win8-deferred.js")
+
+
+Set-ItemTypeNone $jQuerySource
+Set-ItemTypeNone $jQueryVsdoc
+Set-ItemTypeNone $jQueryDeferredSource
